7647e853f3501948d04d81a6a7f2a8a2a52e7ffc
Trixie Diane Bautista <tbautista@diavox.net>
2023-05-06 13:41:04 +0800

    Conditional Rendering (did not use ternary operator)

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,20 @@
 // import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
   return (
-    // <Fragment>
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
         ))}
       </ul>
     </>
-    // </Fragment>
   );
 }
 
